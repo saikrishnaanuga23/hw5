061cd4de7791c4008e7be5a32c0ef44dfc535b08
saikrishnaanuga23 <146044657+saikrishnaanuga23@users.noreply.github.com>
2024-04-23 13:53:32 -0500

    changes made

--- src/view/view.jsx
+++ src/view/view.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const View = ({ activity, onNewActivity }) => {
   return (
     <div>
-      <h1>Feeling Bored?</h1>
+      <h1>Don't know what to do</h1>
       <button onClick={onNewActivity}>Get New Activity</button>
       <p>{activity}</p>
     </div>
